a8aaabf490019ac9c0ea76d584d438fd36493a87
omar jandali <omarjandali93@gmail.com>
2023-06-19 20:47:21 -0700

    Omar Jandali. Updated Files

--- src/Screens/PropertiesScreen.js
+++ src/Screens/PropertiesScreen.js
@@ -13,7 +13,7 @@ import { useNavigation } from '@react-navigation/native'
 import { auth } from '../Api/firebaseTesting'
 
 //this is the firs tthing and this si going to be
-//helloasdfasdf  sxc
+//helloasdfasdf  sxc asdfasdf
 
 const deviceWidth = Dimensions.get('window').width
 const deviceHeight = Dimensions.get('window').height
